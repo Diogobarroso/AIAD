28960c62c5f6cf069236781ac2488cd9464ea43a
RGeneral24 <pedromdcastro@gmail.com>
2015-11-16 15:45:08 +0000

    Config Double Agent run (v1.0) + Starting Message (v0.1)

--- src/com/aiad_schedules/algorithm/ABT/ABT_Main.java
+++ src/com/aiad_schedules/algorithm/ABT/ABT_Main.java
@@ -16,11 +16,20 @@ import java.util.regex.Pattern;
 // Main ABT based Agent Class
 public class ABT_Main extends Agent {
 
+    // ### DEBUG ###
+    static public boolean DEBUG = false;
+    // ### DEBUG ###
+
     // Variables
     public ABT ABT_Agent = new ABT();
 
     // Internal Behaviour Classes
+
+    // ### TESTING ### //
+    // Simple Test Message
     class ABT_Behaviour extends SimpleBehaviour {
+
+        protected int counter = 0;
 
         // Constructor
         public ABT_Behaviour(Agent a) {
@@ -31,6 +40,7 @@ public class ABT_Main extends Agent {
         // Behaviour Action
         public void action(){
 
+            //ACLMessage msg = receive();
             ACLMessage msg = blockingReceive();
 
             if(msg.getPerformative() == ACLMessage.INFORM) {
@@ -43,22 +53,21 @@ public class ABT_Main extends Agent {
             if(msg.getContent().equals("hello")){
 
                 reply.setContent("hi");
-            }
-            else {
+                send(reply);
 
-                reply.setContent("hello");
+                counter++;
             }
-
-            send(reply);
         }
 
         // Behaviour Finish
         public boolean done() {
 
-            return true;
+            return counter == 1;
+            //return true;
         }
 
     }
+    // ### TESTING ### //
 
     // Agent Setup Operations
     protected void setup() {
@@ -75,16 +84,16 @@ public class ABT_Main extends Agent {
         }
         catch (IOException e){
 
-            e.printStackTrace();
             doDelete();
+            e.printStackTrace();
         }
 
         // Register in DF
         DFAgentDescription dfd = new DFAgentDescription(); // all agent descriptions
         dfd.setName(getAID());
         ServiceDescription sd = new ServiceDescription(); // All service; all functionalities
-        sd.setName(getName());
-        sd.setType(ABT_Agent.getAgentName());
+        sd.setName(getName()); // Describes agent name by JADE name
+        sd.setType(ABT_Agent.getAgentName()); // Describes agent real name on the service
         dfd.addServices(sd);
 
         try {
@@ -92,13 +101,15 @@ public class ABT_Main extends Agent {
             DFService.register(this, dfd);
         } catch(FIPAException e) {
 
+            doDelete();
+            System.err.println("Error Detected! Can't Register Agent!");
             e.printStackTrace();
         }
 
         // Verify if there is an existing agent
         DFAgentDescription equalAgent = new DFAgentDescription();
         ServiceDescription equalAgentName = new ServiceDescription();
-        equalAgentName.setType(ABT_Agent.getAgentName());
+        equalAgentName.setType(ABT_Agent.getAgentName()); // Search on the type
         equalAgent.addServices(equalAgentName);
 
         try {
@@ -112,14 +123,48 @@ public class ABT_Main extends Agent {
         }
         catch(FIPAException e) {
 
-            e.printStackTrace();
             doDelete();
+            e.printStackTrace();
         }
+
+        // ### TESTING ### //
 
         // Creates Sample Behaviour
         ABT_Behaviour b = new ABT_Behaviour(this);
         addBehaviour(b);
 
+        // Reads arguments
+        Object[] args = getArguments();
+
+        // If there are arguments its an initiator agent
+        if(args != null && args.length > 0) {
+
+            String[] arguments = (String[]) args;
+            if(arguments[0].equals("start")){ // TEMP: start sample transaction
+
+                DFAgentDescription targetAgent = new DFAgentDescription();
+                ServiceDescription targetService = new ServiceDescription();
+                targetService.setType("Jeremy"); // base find
+                targetAgent.addServices(targetService);
+
+                ACLMessage msg = new ACLMessage(ACLMessage.INFORM); // msg create
+                try { // try is needed for fipa dependencies
+
+                    DFAgentDescription[] searchAgent = DFService.search(this, targetAgent); // agent find
+
+                    msg.addReceiver(searchAgent[0].getName()); // send to agent
+                    msg.setContent("hello");
+                    send(msg);
+                }
+                catch (FIPAException e) {
+
+                    doDelete();
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // ### TESTING ### //
     }
 
     // Agent Shutdown
